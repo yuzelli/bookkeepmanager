d3a701b6bc6877e5ea333617d615ad93fed596be
yuzelli <zsl5155@qq.com>
2017-08-26 18:02:39 +0800

    add changepass

---
+++ app/src/main/java/com/example/yuzelli/bookkeepmananger/view/activity/ChangePassActivity.java
@@ -0,0 +1,26 @@
+package com.example.yuzelli.bookkeepmananger.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.yuzelli.bookkeepmananger.R;
+import com.example.yuzelli.bookkeepmananger.base.BaseActivity;
+
+public class ChangePassActivity extends BaseActivity {
+
+
+    @Override
+    protected int layoutInit() {
+        return R.layout.activity_change_pass;
+    }
+
+    @Override
+    protected void binEvent() {
+
+    }
+
+    @Override
+    protected void fillData() {
+
+    }
+}
